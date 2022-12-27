b69d7b4058f255dffe5c92b1440fea9ec0907c20
Will Forcey <wsforc3y@gmail.com>
2022-12-27 11:34:45 -0500

    Added starter swords

--- initializeGameData.go
+++ initializeGameData.go
@@ -11,19 +11,27 @@ func InitializeRooms(m *sync.Map) {
 	r2key, r2ptr := newRoom("r2", defaultRoomUpdate, &roomMapData.R2Layout, nil, nil, nil, r1ptr)
 	r2ptr.wallColor = "#442800"
 	r1ptr.aboveRoom = r2ptr
+	r3key, r3ptr := newRoom("r3", defaultRoomUpdate, &roomMapData.R1Layout, nil, nil, r1ptr, nil)
 	m.Store(r1key, r1ptr)
 	m.Store(r2key, r2ptr)
 }
 
 func InitializeEntities(m *sync.Map) {
 	// List all the entities you want here
-	r, _ := m.Load("r1")
+	r, _ := m.Load("r2")
 	tmpR := r.(*Room)
-	tmpR.Entities.StoreEntity(newSword(tmpR, 20, 20))
-	tmpR.Entities.StoreEntity(newSword(tmpR, 20, 40))
+	tmpR.Entities.StoreEntity(newSword(tmpR, 15, 55))
+	tmpR.Entities.StoreEntity(newSword(tmpR, 30, 55))
+	tmpR.Entities.StoreEntity(newSword(tmpR, 45, 55))
+	tmpR.Entities.StoreEntity(newSword(tmpR, 60, 55))
+	tmpR.Entities.StoreEntity(newSword(tmpR, 75, 55))
+	tmpR.Entities.StoreEntity(newSword(tmpR, 90, 55))
+	tmpR.Entities.StoreEntity(newSword(tmpR, 105, 55))
+	tmpR.Entities.StoreEntity(newSword(tmpR, 120, 55))
+	tmpR.Entities.StoreEntity(newSword(tmpR, 135, 55))
 	//tmpR.Entities.StoreEntity(newBat(tmpR, 50, 75))
-	tmpR.Entities.StoreEntity(newBat(tmpR, 10, 15))
-	m.Store("r1", tmpR)
+	// tmpR.Entities.StoreEntity(newBat(tmpR, 10, 15))
+	m.Store("r2", tmpR)
 
 	/*
 	m.Store(newBat(50, 75))
